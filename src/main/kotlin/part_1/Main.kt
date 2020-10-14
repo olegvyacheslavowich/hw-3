6c2414c63555f99d41b88e6e35e3815106b4a1e4
Oleg Karpenko <20o13k@gmail.com>
2020-10-15 01:18:01 +0500

    Initial commit

---
+++ src/main/kotlin/part_1/Main.kt
@@ -0,0 +1,54 @@
+package part_1
+
+fun main() {
+
+    val sec = 150000;
+    print("Был в сети ${agoToText(sec)}")
+
+}
+
+fun agoToText(sec: Int): String {
+
+    val minute = 60;
+    val hour = minute * 60;
+    val day = hour * 24;
+    val day_2 = day * 2;
+    val day_3 = day * 3;
+
+    return when (sec) {
+        in 0..minute -> "только что"
+
+        in minute + 1..hour -> {
+            val currentMinute = sec / 60;
+            val ending = getEnding(time = currentMinute);
+            "$currentMinute $ending назад"
+        }
+        in hour + 1..day -> {
+            val currentHour = sec / (60 * 60);
+            val ending = getEnding(false, currentHour);
+            "$currentHour $ending назад"
+        }
+        in day + 1..day_2 -> "сегодня"
+        in day_2 + 1..day_3 -> "вчера"
+        else -> "давно"
+    }
+}
+
+fun getEnding(isMinute: Boolean = true, time: Int): String {
+    val timeString = time.toString();
+    val lastChar = timeString[timeString.length - 1];
+
+    return if (isMinute) {
+        when (lastChar) {
+            '1' -> "минуту"
+            '2', '3', '4' -> "минуты"
+            else -> "минут"
+        }
+    } else {
+        when (lastChar) {
+            '1' -> "час"
+            '2', '3', '4' -> "часа"
+            else -> "часов"
+        }
+    }
+}
---
+++ src/main/kotlin/part_2/Main.kt
@@ -0,0 +1,39 @@
+package part_2
+
+fun main() {
+
+    val amount = 13000.00;
+    val previousAmount = 100.00;
+    val cartType = "Vk Pay";
+
+    if (checkLimits(cartType, previousAmount, amount)) {
+        print("Привышен лимит перевода")
+        return
+    }
+
+    print("Комиссия за перевод: ${countCommission(cartType, previousAmount, amount)}")
+
+
+}
+
+fun countCommission(cartType: String = "Vk Pay", previousAmount: Double, amount: Double): Double {
+
+    return when (cartType) {
+        "MASTER CARD", "MAESTRO" -> {
+            return when (amount + previousAmount) {
+                in 0.0..75000.00 -> 0.00
+                else -> (amount * 0.06 / 100) + 20.00;
+            }
+        }
+        "VISA", "МИР" -> {
+            val commission = amount * 0.75 / 100
+            return if (commission < 35.00) 35.00 else commission
+        }
+        else -> 0.00
+    }
+
+}
+
+fun checkLimits(cartType: String, previousAmount: Double, amount: Double): Boolean {
+    return if (cartType == "Vk Pay") amount > 15_000.00 || previousAmount + amount > 40_000 else previousAmount + amount > 600_000.00
+}
