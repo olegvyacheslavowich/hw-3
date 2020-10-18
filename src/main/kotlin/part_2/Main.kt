2f79a998251c87d773392cba5cbd1273e26023c0
Oleg Karpenko <20o13k@gmail.com>
2020-10-18 22:39:02 +0500

    Исправления после комментариев от ментора part 2

--- src/main/kotlin/part_2/Main.kt
+++ src/main/kotlin/part_2/Main.kt
@@ -18,16 +18,22 @@ fun main() {
 
 fun countCommission(cartType: String = "Vk Pay", previousAmount: Double, amount: Double): Double {
 
+    val maxAmountMasterCardMaestro = 75000.00
+    val masterCardMaestroCommissionPercent = 0.06
+    val masterCardMaestroCommissionValue = 20.00
+    val visaCommissionPercent = 0.75
+    val visaMinCommissionValue = 35.00
+
     return when (cartType) {
         "MASTER CARD", "MAESTRO" -> {
             return when (amount + previousAmount) {
-                in 0.0..75000.00 -> 0.00
-                else -> (amount * 0.06 / 100) + 20.00;
+                in 0.0..maxAmountMasterCardMaestro -> 0.00
+                else -> (amount * masterCardMaestroCommissionPercent / 100) + masterCardMaestroCommissionValue
             }
         }
         "VISA", "МИР" -> {
-            val commission = amount * 0.75 / 100
-            return if (commission < 35.00) 35.00 else commission
+            val commission = amount * visaCommissionPercent / 100
+            return if (commission < visaMinCommissionValue) visaMinCommissionValue else commission
         }
         else -> 0.00
     }
@@ -35,5 +41,15 @@ fun countCommission(cartType: String = "Vk Pay", previousAmount: Double, amount:
 }
 
 fun checkLimits(cartType: String, previousAmount: Double, amount: Double): Boolean {
-    return if (cartType == "Vk Pay") amount > 15_000.00 || previousAmount + amount > 40_000 else previousAmount + amount > 600_000.00
+
+    val vkMaxAmountPerDay = 15_000.00
+    val vkMaxAmountPerMouth = 40_000.00
+    val maxAmountPerDay = 150_000.00
+    val maxAmountPerMouth = 600_000.00
+
+    return if (cartType == "Vk Pay") {
+        amount > vkMaxAmountPerDay || previousAmount + amount > vkMaxAmountPerMouth
+    } else {
+        amount > maxAmountPerDay || previousAmount + amount > maxAmountPerMouth
+    }
 }
